93a1d4c8ca17b1372d33e1a05a39ec65d8b0a47a
Zarmina Desktop <zarminaehsan@icloud.com>
2022-11-01 13:35:32 +0100

    adding new files from PC

--- Uke1/src/kap1/UkeOppgaver1.java
+++ Uke1/src/kap1/UkeOppgaver1.java
@@ -1,12 +1,18 @@
 package kap1;
 
 /*
-Oppgave 1:
-vi har en min-metode, dvs. en metode som finner (og returnerer) posisjonen til den minste verdien i en tabell.
-Hva ville metodekallet min(a) returnere med hvis a er tabellen i Figur 1.1.2?
+Oppgave 1 og 2 :
+Anta at vi har en min-metode, dvs. en metode som finner (og returnerer) posisjonen til den minste verdien i en tabell.
+Hva ville metodekallet min(a) returnere med hvis a er tabellen i Figur 1.1.2? Lag denne min-metode.
 [8	4	17	10	6	20	1	11	15	3	18	9	2	7	19]
+
 */
 
+/*
+Oppgave 3 :
+Hvis den største verdien forekommer flere ganger, vil maks-metoden returnere posisjonen til den første av dem.
+Hva må endres for at den skal returnere posisjonen til den siste?
+*/
 
 public class UkeOppgaver1 {
 
@@ -28,6 +34,25 @@ public class UkeOppgaver1 {
         return utskrift;
     }
 
+    //Oppgave 3:
+    private static String maks(int[] a){
+
+        int maks = a[0];
+        int posisjon = 0;
+
+        for(int i = 0; i < a.length; i++){
+
+            // erstatter dette < med dette <=
+            if(a[i] >= maks){
+                maks = a[i];
+                posisjon = i;
+            }
+        }
+
+        String utskrift = "Posisjonen til den siste største verdien " + posisjon + " og verdien er " + maks;
+        return utskrift;
+    }
+
 
     public static void main(String[] args){
 
@@ -36,6 +61,11 @@ public class UkeOppgaver1 {
 
         //metodekallet min(a) og skriver ut
         String minstetall = min(a);
-        System.out.print(minstetall);
+        System.out.println(minstetall);
+
+        //metodekallet maks(a) og skriver ut
+        String maks = maks(a);
+        System.out.println(maks);
+
     }
 }
