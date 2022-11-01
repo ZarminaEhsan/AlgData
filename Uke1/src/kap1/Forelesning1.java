93897c8bc713567c8fcb978d69ed5af36e2e9c71
Zarmina Desktop <zarminaehsan@icloud.com>
2022-11-01 15:43:03 +0100

    oppgave 3: alternative way to solve problem

--- Uke1/src/kap1/Forelesning1.java
+++ Uke1/src/kap1/Forelesning1.java
@@ -18,6 +18,20 @@ public class Forelesning1 {
     static int max(int[] a) {
         int max_value = a[0];
         for (int i=1; i<a.length; ++i) {
+
+            /*
+            Dominerende operasjoner Det er ikke vanlig å ta med alle operasjonene når en vurderer
+            en algoritmes effektivitet. Det er ofte slik at én eller noen av operasjonene er viktigere,
+            mer sentrale eller mer kostbare enn de andre. Slike operasjoner kalles dominerende.
+            Eksempel: I maks-metoden er det å sammenligne verdier selve kjernen i algoritmen. De andre
+            operasjonene er mer eller mindre en del av implementasjonen.
+            Det å sammenligne er derfor den dominerende operasjonen. ->  (a[i] > max_value)
+
+            i andre algoritmer kan det være andre typer operasjoner som er dominerende.
+            I numeriske algoritmer (dvs. algoritmer som inneholder tallberegninger) kan f.eks. det å
+            multiplisere eller det å dividere være den dominerende operasjonen.
+             */
+
             if (a[i] > max_value) {
                 max_value = a[i]; //Her gjør vi et nytt oppslag i a - er dette dyrt eller billig?
             }
