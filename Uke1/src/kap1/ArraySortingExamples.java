f5fe8d61c5eda840b31e0fdf5be9eb1878396556
Zarmina Desktop <zarminaehsan@icloud.com>
2022-11-04 12:43:09 +0100

    did some practice about sortering array using 2 loops

--- Uke1/src/kap1/ArraySortingExamples.java
+++ Uke1/src/kap1/ArraySortingExamples.java
@@ -1,11 +1,46 @@
 package kap1;
 
+
+import java.util.Arrays;
+
 public class ArraySortingExamples {
 
     public static void main(String[] args){
 
+        ///creating an instance of an array
+        int[] liste = new int []{9,8,2,1,2,3,4,5};
 
-        // ascending order
+        // ascending order: small to big
+        // example 1: with sort method
+        sort(liste);
+
+
+        // example2: using loop
+        sort1(liste);
 
     }
+
+    public static int[] sort(int[] a){
+
+        Arrays.sort(a);
+
+        // sort() method is of void type thats why kisi variable ko initialize nahi ker sakty with Arrays.sort(liste)
+        // printing out array using loop
+
+        for(int i = 0; i < a.length; i++){
+            System.out.print(a[i] + " ");
+        }
+
+        System.out.println();
+
+        //print out using toString
+        System.out.println(Arrays.toString(a));
+
+        return a;
+    }
+
+    public static int[] sort1(int[] b){
+
+    }
+
 }
