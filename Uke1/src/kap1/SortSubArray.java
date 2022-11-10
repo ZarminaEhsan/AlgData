08e55d82c698858c23e6eb67b2c0ed2bf5a12361
Zarmina Desktop <zarminaehsan@icloud.com>
2022-11-10 12:39:54 +0100

    sorting array examples

--- Uke1/src/kap1/SortSubArray.java
+++ Uke1/src/kap1/SortSubArray.java
@@ -25,7 +25,7 @@ public class SortSubArray {
         int[] a = {12, 90, 34, 2, 45, 3, 22, 18, 5, 78};
 
         // sorts subarray form index 2 to 7
-        Arrays.sort(a, 2, 7);
+        //Arrays.sort(a, 2, 7);
 
         //prints array using the for loop
         for (int i = 0; i < a.length; i++) {
@@ -33,7 +33,6 @@ public class SortSubArray {
         }
 
         System.out.println();
-
 
         // using loop to sort in ascending order
 
@@ -50,7 +49,11 @@ public class SortSubArray {
                 }
             }
         }
-
         System.out.println(Arrays.toString(a));
+
+        // using loop to print array
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
     }
 }
